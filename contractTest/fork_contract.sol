0e05fca1755d93c772e05bc971dd6ad4bcb20c4d
root <root@fabric.gitlab>
2019-05-17 02:36:15 +0800

    update contract

--- contractTest/fork_contract.sol
+++ contractTest/fork_contract.sol
@@ -1,24 +1,22 @@
 contract ForkContract {
     address user;
     mapping(address => uint) public credits;
-    function forkContract() public{
+    function forkContract() payable {
         user = 0x123;
         credits[user] = 10;
-        user = 0x456;
-        credits[user] = 20;
-        user = 0x789;
-        credits[user] = 30;
-
 }
-    function Update() public view returns (uint64){
-                uint64 a = 0;
+    function update() public returns (uint64){
+                uint64 a = 7;
                 user = 0x123;
-		d_storage d = user;
-                a = d.sused(credits[user]);
-                user = 0x456;
-                d = user;
-                a = d.sused(credits[user]);
-		return a;
+                d_storage d = user;
+                a = d.sused(a);
+                d_storage d2 = 0x456;
+                a = 456;
+                a = d2.sused(a);
+                a=8;
+//                d = user;
+//                a = d.sused(credits[user]);
+                return a;
     }
 }
-// ----
+
